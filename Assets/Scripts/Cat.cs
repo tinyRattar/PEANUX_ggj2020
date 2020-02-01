ee72843d98cfec8eff8c69849d642603fb93216c
playground <WPG@bupt.edu.cn>
2020-02-01 23:25:17 +0800

    Update cat.cs tc pattern

--- Assets/Scripts/Cat.cs
+++ Assets/Scripts/Cat.cs
@@ -1,7 +1,6 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 
 public enum CatAttackType
 {
@@ -14,39 +13,39 @@ public enum CatState
 {
     idle,
     walk,
-    attack,
-    beLoved,
-    fooled
+    directlywalk,
+    attack,   
+    beLoved
 }
 
 public class Cat : MonoBehaviour
 {
     [SerializeField]CatState state = CatState.idle;
-    static float catPlayTimerValue = 5.0f;
-
-    float timer = 0f;
-    Vector2 direction = Vector2.zero;
-    float moveTime = 1.0f;
-    float attackTime = 1.0f;
-    int walkedCount = 0;
-    [SerializeField] int fooled_nums = 0;
-    bool started_run_to_player_flag = false;
-    bool neared_player_flag = false;
-    bool is_dice_rolled = false;
+    [SerializeField] float timer = 0f;
+    [SerializeField] Vector2 direction = Vector2.zero;
+    [SerializeField] float moveTime = 1.0f;
+    [SerializeField] float attackTime = 1.0f;
+    [SerializeField] int walkedCount = 0;
     GameObject targetAttack;
-    [SerializeField] int leastRandomWalk = 4;
+    [SerializeField] int leastRandomWalk = 2;
     [SerializeField] float attackProb = 0.8f;
     
     //[SerializeField] float baseProb = 0.4f;
-    [SerializeField] float perWalkProb = 0.2f;
-    [SerializeField] float restProb = 0.2f;
-    [SerializeField] float moveSpeed = 1.0f;
+    [SerializeField] float perWalkProb = 0.5f;
+    [SerializeField] float restProb = 0.3f;
+    [SerializeField] float moveSpeed = 4.0f;
     [SerializeField] float attackRange = 1.0f;
-    [SerializeField] float catPlayTimer = catPlayTimerValue;
+    [SerializeField] float directlyWalkProb = 0.5f;
+
+    [SerializeField] Transform furniture;
+    int furnitureCount;
+
     // Start is called before the first frame update
     void Start()
     {
         NextActionDecide();
+        furniture = GameObject.Find("Furniture").transform;
+        furnitureCount = furniture.childCount;
     }
 
     // cat behave
@@ -69,7 +68,8 @@ public class Cat : MonoBehaviour
         if (walkedCount >= leastRandomWalk)
         {
             float p = Random.Range(0.5f, 1f);
-            r1 = walkedCount * perWalkProb * p;
+            p = 1f;
+            r1 = (1 + walkedCount) * perWalkProb * p;
         }
         if (r1 > attackProb)
         {
@@ -80,18 +80,33 @@ public class Cat : MonoBehaviour
             }
         }
         // if didn't attack
-        if (r2 >= restProb)
+        if (r2 > restProb)
         {
-            float angle = Random.Range(0, 360);
-            direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
-            moveTime = Random.Range(1f, 2f);
+            float r3 = Random.Range(0f, 1f);
+
+            if (r3 >= directlyWalkProb)
+            {
+                float angle = Random.Range(0, 360);
+                direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+                moveTime = Random.Range(1f, 2f);
+                state = CatState.walk;
+            }
+            else
+            {
+                Vector3 nextFurniturePosition = furniture.GetChild(Random.Range(0, furnitureCount - 1)).position;
+                moveTime = (nextFurniturePosition - transform.position).magnitude / moveSpeed;
+                direction = (nextFurniturePosition - transform.position) / (moveSpeed * moveTime);
+                state = CatState.directlywalk;
+            }
+
             timer = moveTime;
-            state = CatState.walk;
+            
             walkedCount++;
+
         }
         else
         {
-            float idleTime = Random.Range(1f, 2f);
+            float idleTime = Random.Range(0.5f, 1.5f);
             timer = idleTime;
             state = CatState.idle;
         }
@@ -124,9 +139,8 @@ public class Cat : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        CheckFooled();
         switch (state)
         {
             case CatState.idle:
@@ -135,17 +149,14 @@ public class Cat : MonoBehaviour
             case CatState.walk:
                 CatWalk();
                 break;
+            case CatState.directlywalk:
+                CatWalk();
+                break;
             case CatState.attack:
                 // attack timer
                 // if attackover then NextActionDecide()
                 break;
             case CatState.beLoved:
-                OnBeLoved();
-                Debug.Log("In Love");
-                return;
-                break;
-            case CatState.fooled:
-                UpdateFoolNums();
                 break;
             default:
                 break;
@@ -153,94 +164,13 @@ public class Cat : MonoBehaviour
         timer -= Time.deltaTime;
         if (timer < 0)
             NextActionDecide();
-
-        // 如果人在逗猫
-        if (PlayerCTRL.instance.GetState() == playerState.summonCat){
-            // var ignore_probability = 0.0f + fooled_nums*0.2;
-            // if(ignore_probability >=0.8){
-            //     ignore_probability=0.8;
-            // }
-            // var roll_dice = Random.Range(0.0f,1.0f) + 0.001f;
-            // if(roll_dice<ignore_probability){
-            //     is_dice_rolled = true;
-            //     PlayerCTRL.instance.SetState(playerState.idle);
-            //     // 被无视了
-            //     return;
-            // }
-            
-            
-            if(!NearPlayer(PlayerCTRL.instance.gameObject.transform.position)){
-                RunToPlayer(PlayerCTRL.instance.gameObject.transform.position);
-
-            }
-            else{
-                catPlayTimer = catPlayTimerValue;
-                PlayerCTRL.instance.club_num--;
-                PlayingWithPlayer(PlayerCTRL.instance.cur_tool);
-                fooled_nums = 0;
-            }
-        }
     }
 
-    void CheckFooled(){
-        // if(started_run_to_player_flag==true && neared_player_flag==false){
-        //     state = CatState.fooled;
-        // }
-    }
-
-    void UpdateFoolNums(){
-        // fooled_nums++;
-        // started_run_to_player_flag = false;
-        // neared_player_flag = false;
-        // state = CatState.idle;
-    }
-    bool NearPlayer(Vector3 playerPos){
-        Vector2 desPos = playerPos;
-        Vector2 curPos = this.transform.position;
-        var distance = (desPos-curPos).magnitude;
-        if(distance < 0.6){
-            return true;
-        }
-        return false;
-    }
-
-    void RunToPlayer(Vector3 playerPos){
-        started_run_to_player_flag = true;
-        Vector2 desPos = playerPos;
-        Vector2 curPos = this.transform.position;
-        Vector2 direction = (desPos-curPos).normalized;
-        Vector2 nextPos = curPos + direction*4*moveSpeed*Time.deltaTime;
-        
-        this.transform.position = nextPos;
-    }
-
-    void PlayingWithPlayer(ToolType tool){
-        neared_player_flag = true;
-
-        if (tool == ToolType.catToy){
-            // 猫叫
-
-            // 放动画
-            state = CatState.beLoved;
-        }
-        else{
-            return;
-        }
-        
-        // start meowing 
-
-        
-    }
     public void OnBeLoved()
     {
-        PlayerCTRL.instance.SetState(playerState.servingCat);
-        // change timer
-        catPlayTimer = catPlayTimer-Time.deltaTime;
-        if(catPlayTimer < 0){
-            state = CatState.walk;
-            PlayerCTRL.instance.SetState(playerState.idle);
-            PlayerCTRL.instance.cur_tool = ToolType.empty;
-            Debug.Log("idle player");
+        if (state != CatState.attack)
+        {
+            state = CatState.beLoved;
         }
     }
 
